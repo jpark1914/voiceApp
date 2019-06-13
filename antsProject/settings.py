2b56305d384bd39e95000255b33c4a3fde27c8eb
jpark1914 <parkerj4321@gmail.com>
2019-06-13 18:23:16 -0500

    Got everything accesbile via 'env'

--- antsProject/settings.py
+++ antsProject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crudSQL.apps.CrudsqlConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,13 @@ WSGI_APPLICATION = 'antsProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'testdatabase',
+        'USER': 'root',
+        'PASSWORD': 'Park7937!',
+        'HOST': 'localhost',
+        'PORT': '3306',
+
     }
 }
 
